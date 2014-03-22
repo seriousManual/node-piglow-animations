952cad4c21aba145d9f716d8fa370f88fc73a020
zaphod1984 <zaphod84@gmx.de>
2014-03-23 00:31:24 +0100

    compatability steps

--- test.js
+++ test.js
@@ -2,18 +2,17 @@ var piglow = require('piglow');
 
 var animation = require('./');
 var pi = piglow.piGlowInterface;
-var backend = new piglow.BackendMock();
+var backend = new piglow.BackendMockPrettyPrint();
 
 //TODO: depugger, split by newline, add prefix to every single line
 
-animation({interval:10, debug: true}, backend)
-    .set().to(pi(['ring_0'])).after('0.1s')
-    .set().to(pi(['ring_1'])).after('0.1s')
-    .set().to(pi(['ring_2'])).after('0.1s')
-    .fade().to(pi['leg_0']).after('1s').in('1s')
-    .fade().to(pi['leg_1']).after('1s').in('1s')
-    .fade().to(pi['leg_2']).after('1s').in('1s')
-    .repeat(3)
+animation({interval:10, debug: false}, backend)
+    .set().to(pi(['ring_0'])).after('1s')
+    .set().to(pi(['ring_1'])).after('1s')
+    .set().to(pi(['ring_2'])).after('1s')
+    .set().to(pi(['ring_3'])).after('1s')
+    .set().to(pi(['ring_4'])).after('1s')
+    .set().to(pi(['ring_5'])).after('1s')
     .start(function() {
         console.log('i looped 3 times, now Im done.');
     });

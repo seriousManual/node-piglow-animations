4bed4545b09cc41cab2a2e94f514a289280d2086
zaphod1984 <zaphod84@gmx.de>
2014-03-23 12:34:16 +0100

    examples

--- test2.js
+++ test2.js
@@ -7,13 +7,13 @@ var backend = new piglow.BackendMockPrettyPrint();
 //TODO: depugger, split by newline, add prefix to every single line
 
 animation({interval:10, debug: false}, backend)
-    .set().to(pi(['ring_0'])).after('0.1s')
-    .set().to(pi(['ring_1'])).after('0.1s')
-    .set().to(pi(['ring_2'])).after('0.1s')
-    .fade().to(pi['leg_0']).after('1s').in('1s')
-    .fade().to(pi['leg_1']).after('1s').in('1s')
-    .fade().to(pi['leg_2']).after('1s').in('1s')
-    .repeat(3)
+    .set().to(pi(['ring_0'])).after('1s')
+    .set().to(pi(['ring_1'])).after('1s')
+    .set().to(pi(['ring_2'])).after('1s')
+    .fade().to(pi(['leg_0'])).in('1s')
+    .fade().to(pi(['leg_1'])).in('1s')
+    .fade().to(pi(['leg_2'])).in('1s')
+
     .start(function() {
         console.log('i looped 3 times, now Im done.');
     });
